a7f821cdb756543d57fd1d3c2b6ae690cfffe165
Ibrahim <ibrahim.mneimneh0@gmail.com>
2023-11-11 19:02:35 +0200

    User Verification update

--- Controllers/userController.js
+++ Controllers/userController.js
@@ -35,7 +35,13 @@ const loginUser = async (req, res) => {
           pin: hashedPin,
         });
         await emailSender(user.email, "Email Verification", user.username, pin);
-        return res.status(200).json({ token });
+        return res
+          .status(200)
+          .json({
+            token,
+            is2FAEnabled: user.is2FAEnabled,
+            isBiometricAuthEnabled: user.isBiometricAuthEnabled,
+          });
       }
       // we check if we sent him an email but its expired
       if (userAuth.expiresIn < Date.now()) {
